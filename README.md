201eaf1c985463d6cb050cb1fa2bae2322a209eb
Andrii Prashcharuk <monext09@gmail.com>
2018-09-02 12:03:41 +0300

    server for sending email has been implemented

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # image-free-website-server
 Server for Image Free Website
+
+to start server run
+```
+API_KEY=YOUR_MAILGUN_API_KEY DOMAIN=YOUR_DOMAIN EMAIL=your@email.com npm run start
+```
---
+++ src/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const nodeMailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+const {
+    API_KEY,
+    DOMAIN,
+    EMAIL,
+} = process.env;
+
+if (!API_KEY) {
+    throw Error('API_KEY environment variable is not defined');
+}
+if (!DOMAIN) {
+    throw Error('DOMAIN environment variable is not defined');
+}
+if (!EMAIL) {
+    throw Error('EMAIL environment variable is not defined');
+}
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/sendMessage', function (req, res) {
+    const { name = '', email = '', message = '' } = req.body;
+    const senderName = name.trim();
+    const senderEmail = email.trim();
+    const emailText = message.trim();
+
+    if (!senderName || !senderEmail || !emailText) {
+        res.status(400).json({ error: 'name, email or message parameter is empty or missing' });
+    }
+
+    const auth = {
+        api_key: API_KEY,
+        domain: DOMAIN,
+    };
+    const nodeMailerMailgun = nodeMailer.createTransport(mg({ auth }));
+    const mailOptions = {
+        from: `"${senderName}" <${senderEmail}>`,
+        to: EMAIL,
+        subject: `IFW. Message from ${senderName}`,
+        text: emailText,
+    };
+
+    nodeMailerMailgun.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.status(500).json({ error });
+            console.log(error);
+        } else {
+            res.sendStatus(200);
+            console.log('Message %s sent', info.messageId);
+        }
+    });
+});
+
+app.listen(port, () => console.log('Server is running at port: ',port));
