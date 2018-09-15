2a9e8670740052afe7bd6f2a664d0ecd654b60b6
Andrii Prashcharuk <monext09@gmail.com>
2018-09-15 15:37:59 +0300

    - profile image has been added - meta tags for SEO has been added

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,6 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
+const PACKAGE = require('./package.json');
+const version = PACKAGE.version;
 
 const BUILD_DIR = path.resolve(__dirname, '../dst');
 const APP_DIR = path.resolve(__dirname, './');
@@ -74,12 +76,17 @@ module.exports = (env, argv) => {
             new HtmlWebpackPlugin({
                 filename: 'index.html',
                 template: './index.html',
-                scriptPath: `/${scriptName}`,
+                scriptPath: `/${scriptName}?v${version}`,
+                title: 'Andrii Prashcharuk | Image-Free Website',
+                description: 'My name is Andrii Prashcharuk and this is my personal website! I am Professional Software Engineer from Ukraine with more than 7 years of experience in Front-End development.',
+                keywords: 'Prashcharuk, Andrii Prashcharuk, front-end, developer, engineer, react, redux, html5, css3, css3 animation, image-free website',
+                url: 'https://prashchar.uk',
+                image: 'https://prashchar.uk/profile_image.jpg',
                 inject: false,
             }),
             new CopyWebpackPlugin([
                 {
-                    from: './favicon',
+                    from: './static',
                     to: '../dst',
                     toType: 'dir',
                 },
