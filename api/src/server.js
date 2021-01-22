adb639f97bc2f095b17f744c82741b819d3fb462
Andrii Prashcharuk <monext09@gmail.com>
2021-01-22 09:14:19 +0200

    fix: caching html issue chore: add "repository" field and "private" flag to api's package.json

--- api/src/server.js
+++ api/src/server.js
@@ -27,7 +27,13 @@ if (!EMAIL) {
 const isEmailValid = email => (/^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/).test(email);
 
 app.use(compression());
-app.use(express.static(PUBLIC_PATH));
+app.use(express.static(PUBLIC_PATH, {
+    setHeaders: (res, path) => {
+        if (path.endsWith('.html')) {
+            res.setHeader('Cache-Control', 'no-cache');
+        }
+    },
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
